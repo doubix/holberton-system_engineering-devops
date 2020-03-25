34dbf67d0b582950a6b2dfb4cc3e97f1dc027c05
doubix <adib.grouz@gmail.com>
2020-03-25 19:33:28 +0100

    [update] puppet command

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,5 +6,5 @@
 
 exec { 'install':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=EshjSGSg0q0&t=35s permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  command  => 'sudo apt-get -y update ; sudo apt-get -y upgrade ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=EshjSGSg0q0&t=35s permanent;/" /etc/nginx/sites-available/default ; sudo service nginx restart ; sudo service nginx reload',
 }
