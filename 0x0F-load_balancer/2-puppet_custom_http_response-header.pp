a1a93079a905fa53d86bae1e3826863f704a52cd
doubix <adib.grouz@gmail.com>
2020-04-04 02:14:26 +0100

    [update puppet commnad]

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 # creating a custom HTTP header response, but with Puppet.
 #
 #    The name of the custom HTTP header must be X-Served-By
@@ -6,31 +5,28 @@
 #    Write 2-puppet_custom_http_response-header.pp so that it configures a brand new Ubuntu machine to the requirements asked in this task
 
 
-exec {'update':
-  command  => 'sudo apt-get -y update',
-  path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  provider => shell,
-  }
-exec {'upgrade':
-  command  => 'sudo apt-get -y upgrade',
-  path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  provider => shell,
-  } 
-exec {'install':
-  command  => 'sudo apt-get -y install nginx',
-  path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  provider => shell,
-  } 
-
--> file_line { 'change header':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => "	location / {
-  add_header X-Served-By ${hostname};",
-  match  => '^\tlocation / {',
+exec { 'update_packages':
+command => 'sudo apt-get -y update',
+provider => 'shell',
 }
-exec {'restart':
-  command  => 'sudo service nginx restart',
-  path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  provider => shell,
-  }
+
+exec { 'install_nginx':
+command => 'sudo apt-get -y install nginx',
+provider => 'shell',
+}
+
+exec { 'start_nginx':
+command => 'sudo service nginx start',
+provider => 'shell',
+}
+
+exec { 'modify_nginx_config':
+command => 'sed -i -e "/sendfile/i \\\tadd_header X-Served-By \$hostname;" /etc/nginx/nginx.conf',
+provider => 'shell',
+}
+
+exec { 'restart_nginx':
+command => 'sudo service nginx restart',
+provider => 'shell',
+}
+
