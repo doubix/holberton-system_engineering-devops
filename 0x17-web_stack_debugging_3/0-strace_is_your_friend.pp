775751acf1c58c30494f756c47e8138519babcbb
root <adib.grouz@gmail.com>
2020-05-20 19:59:22 +0100

    [add] provider & path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,7 @@
 # fixes Apache 500 error
 exec { 'fix':
   onlyif  => 'test -e /var/www/html/wp-settings.php',
+  provider => 'shell',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
 }
