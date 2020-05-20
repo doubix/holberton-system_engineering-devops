69f277294fb937cf9d49c2a97682288f10c2d24a
root <adib.grouz@gmail.com>
2020-05-20 20:02:57 +0100

    [add] provider & path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fixes Apache 500 error
-exec { 'fix':
+exec { 'fix WP':
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   provider => 'shell',
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
